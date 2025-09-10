8cf317a4a5d458799c8b17b2226e06a47968e9df
kavinraj17 <kavinrajsivasamy@gmail.com>
2025-09-10 18:52:09 +0530

    Initial commit

--- src/main/java/org/example/intelligenceaiapplication/controller/CodeAnalysisController.java
+++ src/main/java/org/example/intelligenceaiapplication/controller/CodeAnalysisController.java
@@ -3,3 +3,5 @@ package org.example.intelligenceaiapplication.controller;
 public class CodeAnalysisController {
 
 }
+git remote add origin https://github.com/kavinraj17/intelligenceAi-Springboot.git
+
